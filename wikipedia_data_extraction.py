8dae651a24af4ca62b5643a5b196882ac0cbe53c
Shivansh Galav <116233981+shiv4223@users.noreply.github.com>
2025-03-20 09:43:36 +0530

    Update wikipedia_data_extraction.py

--- wikipedia_data_extraction.py
+++ wikipedia_data_extraction.py
@@ -1,7 +1,7 @@
 import wikipediaapi
 import chromadb
 
-wiki = wikipediaapi.Wikipedia("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",'en')
+wiki = wikipediaapi.Wikipedia(os.getenv("YOUR_USER_AGENT"),'en')
 
 chroma_client = chromadb.Client()
 collection = chroma_client.get_or_create_collection(name="wiki_knowledge")
